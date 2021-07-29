84511b815d188bac4dd4ddb2c6f018993dcc0521
JustinBlue26 <77999823+JustinBlue26@users.noreply.github.com>
2021-07-29 18:20:27 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+var fireDragon = dragon(fire);
+var iceDragon = dragon(ice);
+var sandDragon = dragon(sand);
+var fogDragon = dragon(fog);
+var runDragon = dragon(run);
+
+
+function click1(amount) {
+  clickNumber1 = clickNumber1 + amount;
+  document.getElementById("Clicker1").innerHTML = clickNumber1;
+  
+}
+
+function click2(amount) {
+  clickNumber2 = clickNumber2 + amount;
+  document.getElementById("Clicker2").innerHTML = clickNumber2;
+  
+}
